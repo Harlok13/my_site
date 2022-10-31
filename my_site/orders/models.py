0195393b82f8675c481c7b1ba09040414de9ddbf
Harlok <holmik90@yandex.ru>
2022-10-31 16:27:50 +0300

    добавлена связь User с SalesOrder

--- my_site/orders/models.py
+++ my_site/orders/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class SalesOrder(models.Model):
-    amounts = models.IntegerField()
+    amount = models.IntegerField()
     descriptions = models.CharField(max_length=255)
     color = models.CharField(max_length=255, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
