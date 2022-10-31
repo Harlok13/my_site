83cbba95807eed1ee18220e86fe990f50ab6d14b
Harlok <holmik90@yandex.ru>
2022-10-31 18:53:11 +0300

    добавлена модель Product и ее связь с SalesOrder

--- my_site/orders/models.py
+++ my_site/orders/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from products.models import Product
+
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     descriptions = models.CharField(max_length=255)
     color = models.CharField(max_length=255, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    product = models.ManyToManyField(Product)
 
